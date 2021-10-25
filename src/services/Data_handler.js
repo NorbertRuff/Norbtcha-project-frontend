f7a6cc2488582d4e19c0fc2cfb56096783857cb6
NorbertRuff <ruffnorbert88@gmail.com>
2021-10-25 15:42:45 +0200

    [update] data handler

--- src/services/Data_handler.js
+++ src/services/Data_handler.js
@@ -41,46 +41,8 @@ export let dataHandler = {
                 }
             });
     },
-    _api_get_selectOptions: function (url, callback, callbackOfTheCallback, errorCallback, loadingCallback) {
-        axios
-            .get(url, {
-                method: 'GET',
-                credentials: 'same-origin'
-            })
-            .then((response) => {
-                callback(response.data, callbackOfTheCallback);
-            })
-            .catch((error) => {
-                errorCallback(error.message);
-                console.error(
-                    `The request was made and the server responded
-        with a status code that falls out of the range of 2xx ` + error.message
-                );
-            })
-            .finally(() => {
-                if (loadingCallback !== undefined) {
-                    loadingCallback(false)
-                }
-            });
-    },
-
-    _api_get_results: function (url, callback, errorCallback) {
-        axios
-            .get(url)
-            .then((response) => {
-                callback(response.data.results);
-            })
-            .catch((error) => {
-                errorCallback(error.message);
-                console.error(
-                    `The request was made and the server responded
-        with a status code that falls out of the range of 2xx ` + error.message
-                );
-            });
-
-    },
-    _api_post: function (url, data, callback, errorCallback) {
-        axios
+    _api_post: async function (url, data) {
+        let result = await axios
             .post(url, data, {
                 method: 'POST',
                 credentials: 'same-origin',
@@ -90,27 +52,14 @@ export let dataHandler = {
                 }
             })
             .then(response => {
-                if (callback !== undefined) {
-                    callback(response.data);
-                }
+                return response;
             })
             .catch((error) => {
-                if (errorCallback !== undefined) {
-                    errorCallback(error.message);
-                }
                 console.error(
                     `The request was made and the server responded with a status code that falls out of the range of 2xx `
                     + error.message
                 );
             })
-    },
-
-    _api_delete: function (url, data) {
-        axios.delete(url, {
-            headers: {
-                Authorization: getLocalstorage(),
-            },
-            data
-        });
+        return result.data
     },
 }
